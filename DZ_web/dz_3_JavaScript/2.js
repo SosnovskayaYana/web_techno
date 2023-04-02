a513e5303cb0f5e340930fc056628239e1eb591b
Sosnovskaya_Yana <114911161+SosnovskayaYana@users.noreply.github.com>
2023-04-02 23:26:50 +0300

    Test
    2.js

--- DZ_web/dz_3_JavaScript/2.js
+++ DZ_web/dz_3_JavaScript/2.js
@@ -5,14 +5,13 @@
 const userButton = document.querySelector('button');
 
 userButton.addEventListener('click', function () {
-
     const userName = prompt("Введите Ваше имя ");
     function greeting(name) {
         alert(`Привет, ${name}! Рады тебя видеть!`);
     }
 
     greeting(userName);
-})
+});
 
 
 // function greeting(name) {
