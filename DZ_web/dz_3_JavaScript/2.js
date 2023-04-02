fc03fc3afaa3c648e7132c8b1a57e663ab8d2b9a
Yana <yan4ik999@yandex.ru>
2023-04-02 13:49:22 +0300

    dz_3_js

--- DZ_web/dz_3_JavaScript/2.js
+++ DZ_web/dz_3_JavaScript/2.js
@@ -2,11 +2,23 @@
 // Cоздать функцию greeting, которая принимает имя и выводит приветствие, используя переданное имя, в консоль.
 // Необходимо у пользователя запросить имя и вызвать функцию greeting, передав туда данное значение.
 
+const userButton = document.querySelector('button');
 
-function greeting(name) {
-    alert(`Привет, ${name}! Рады тебя видеть!`);
-}
+userButton.addEventListener('click', function () {
 
-const userName = prompt("Введите Ваше имя ");
+    const userName = prompt("Введите Ваше имя ");
+    function greeting(name) {
+        alert(`Привет, ${name}! Рады тебя видеть!`);
+    }
 
-greeting(userName);
+    greeting(userName);
+})
+
+
+// function greeting(name) {
+//     alert(`Привет, ${name}! Рады тебя видеть!`);
+// }
+
+// const userName = prompt("Введите Ваше имя ");
+
+// greeting(userName);
