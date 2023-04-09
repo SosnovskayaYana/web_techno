f48831d170a11db78fed601b1cc8c97119cc3f54
Yana <yan4ik999@yandex.ru>
2023-04-09 18:52:20 +0300

    sem_5_web_work_json

--- DZ_web/dz_2_html_css/java_script_1.js
+++ DZ_web/dz_2_html_css/java_script_1.js
@@ -1,7 +1,7 @@
 
-const userButton = document.querySelector('button');
+const userButton1 = document.querySelector('.button1');     /* обращаемся к кнопке по ранее придуманному классу */
 
-userButton.addEventListener('click', function () {
+userButton1.addEventListener('click', function () {
     const userName = prompt("Введите Ваше имя ");
     function greeting(name) {
         alert(`Привет, ${name}! Рады тебя видеть!`);
@@ -9,3 +9,17 @@ userButton.addEventListener('click', function () {
     greeting(userName);
 });
 
+
+
+const userButton2 = document.querySelector('.button2');
+
+userButton2.addEventListener('click', function () {
+    alert(`ДОБЫЧА НЕРУДНЫХ СТРОИТЕЛЬНЫХ МАТЕРИАЛОВ 
+    Для добычи и обработки нерудных материалов спользуются горные породы. 
+    Перед тем, как начинается добыча в промышленных масштабах, 
+    геологоразведочных организации определяют, где расположены нерудные 
+    материалы, каковы масштабы и глубина залежей. Так же они опредяляют 
+    наиболее предпочтительный способ добычи. 
+    Наиболее популярна добыча необходимого сырья с помощью открытых 
+    карьеров.`);
+});
